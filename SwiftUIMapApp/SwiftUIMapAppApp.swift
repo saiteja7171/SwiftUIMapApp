4cc435c4a646ea05b5fdff387e16cedc519e4b0f
saiteja7171 <saiteja7171@gmail.com>
2024-06-18 19:17:37 -0700

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Sai Teja Atluri on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
